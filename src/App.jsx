5a2f64e077c8d674795fc13f9ef08db382e9b4d4
Mariyamalikho <Mariyam.suk22@gmail.com>
2026-04-29 08:42:37 +0000

    feat: add base app structure

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { AuthProvider } from './context/AuthContext';
+import { JournalProvider } from './context/JournalContext';
+import { Navbar } from './components/Navbar';
+import { BookContainer } from './components/Book/BookContainer';
+
+function App() {
+  return (
+    <AuthProvider>
+      <JournalProvider>
+        <div className="min-h-screen font-body antialiased" style={{ background: 'var(--bg)', color: 'var(--text-primary)' }}>
+          <Navbar />
+          <BookContainer />
+        </div>
+      </JournalProvider>
+    </AuthProvider>
+  );
+}
+
+export default App;
