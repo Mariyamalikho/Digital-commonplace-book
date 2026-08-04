b6a4f77f05765d890fd8409bf76d4ae5260fd446
Mariyamalikho <Mariyam.suk22@gmail.com>
2026-08-05 03:25:08 +0500

    style: enforce 2-space indentation in App.jsx. Fixes #12

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
-import { AuthProvider } from './context/AuthContext';
-import { JournalProvider } from './context/JournalContext';
-import { Navbar } from './components/Navbar';
-import { BookContainer } from './components/Book/BookContainer';
+import React from "react";
+import { AuthProvider } from "./context/AuthContext";
+import { JournalProvider } from "./context/JournalContext";
+import { Navbar } from "./components/Navbar";
+import { BookContainer } from "./components/Book/BookContainer";
 
 function App() {
   return (
     <AuthProvider>
       <JournalProvider>
-        <div className="min-h-screen font-body antialiased" style={{ background: 'var(--bg)', color: 'var(--text-primary)' }}>
+        <div
+          className="min-h-screen font-body antialiased"
+          style={{ background: "var(--bg)", color: "var(--text-primary)" }}
+        >
           <Navbar />
           <BookContainer />
         </div>
